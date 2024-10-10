2eec00992572adc344d4e0afb1dd7196b6f72b49
Glenn Roberts <me@glenn-roberts.com>
2024-10-10 23:04:38 +1000

    slack

--- install/desktop/optional/app-slack.sh
+++ install/desktop/optional/app-slack.sh
@@ -1,1 +1,5 @@
-sudo apt-get install slack-desktop
+cd /tmp
+rm -f ./slack-desktop-*.deb
+wget https://downloads.slack-edge.com/desktop-releases/linux/x64/4.39.95/slack-desktop-4.39.95-amd64.deb
+sudo apt install ./slack-desktop-*.deb
+cd -
